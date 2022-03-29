8f4a8266898d8386e3c4c71f22be68ded3819399
orlandogurrola <orlandokevin.gurrola@gmail.com>
2022-03-28 21:47:29 -0700

    Awesome Project correction

---
+++ AwesomeProject/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.container}>CETYS UNIVERSIDAD</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'right',
+    justifyContent: 'left',
+    fontSize: 90
+  },
+});
